01846aa1618264330de9fe3868ce3107df38b9f6
unknown <khatiwada.saurabh@gmail.com>
2020-06-19 21:38:14 -0400

    Fixing some formatting

--- src/pages/registration.js
+++ src/pages/registration.js
@@ -5,13 +5,15 @@ export default class Pricing extends React.Component {
     render() {
         const body = {
             height: '842px',
-            width: '625px',
-            marginLeft: 'auto',
-            marginRight: 'auto',
+            width: '800px',
+            marginLeft: '50px'
         }
         const alignText = {
             textAlign: 'center',
             marginBottom: '0'
+        }
+        const table = {
+            marginLeft: '20px'
         }
         const boldFont = {
             fontWeight: 'bold'
@@ -21,100 +23,131 @@ export default class Pricing extends React.Component {
             outline: '0',
             background: 'transparent',
             borderBottom: '2px solid black',
-            marginBottom: '5px'
+            marginBottom: '5px',
+            flex: '1'
+        }
+        const trStyle = {
+            marginBottom: '200px'
+        }
+        const flexContainer = {
+            width: '750px',
+            display: 'flex'
+        }
+        const fillWidth = {
+            flex: '1'
         }
         return (
             <div style={body}>
                 <div style={alignText}>
                     <div style={boldFont}>
                         <br />
-                        <h4> BBB Auto School
+                        <h4 style={{ marginRight: '12px' }}> BBB Auto School
                             <br />
                             Taunton, MA 02780
                             <br />
                             Tel: (508)-880-3333
                             <br style={{ marginBottom: '5px' }} />
-                            Registration Contract
+                            REGISTRATION CONTRACT <br />
                         </h4>
                     </div>
                 </div>
-
-                <div style={{ marginTop: '5px' }} >
-
-                    Student's Name: &nbsp; <input style={inputStyle} type="text" size='50' /> <br />
-
-		Parent's Name: &nbsp; <input style={inputStyle} type="text" size='51' /> <br />
-
-		Address: &nbsp; <input style={inputStyle} type="text" size='57' /> <br />
-		City: &nbsp; <input style={inputStyle} type="text" size='20' />
-		State: &nbsp; <input style={inputStyle} type="text" size='12' />
-		Zip Code: &nbsp; <input style={inputStyle} type="text" size='6' /> <br />
-
-		Student's Email Address: &nbsp; <input style={inputStyle} type="text" size='44' /> <br />
-
-		Home Phone: &nbsp; <input style={inputStyle} type="text" size='15' />
-		&nbsp; Student's Cell Phone: <input style={inputStyle} type="text" size='16' /><br />
-
-		Best Time To Call: &nbsp; <input style={inputStyle} type="text" size='49' /> <br />
-
-
-		Parent's Phone No: &nbsp; <input style={inputStyle} type="text" size='48' /> <br />
-
-
-		Learner's Permit #: &nbsp; <input style={inputStyle} type="text" size='48' /> <br />
-
-		Date of Birth: &nbsp; <input style={inputStyle} type="text" size='18' />
-		&nbsp;  Starting Date: <input style={inputStyle} type="text" size='18' /> <br />
-
-		Notes: &nbsp; <input style={inputStyle} type="text" size='57' />
+                <div style={{ marginTop: '10px', width: '100%' }} >
                     <br />
-                    <p style={{ marginTop: '5px', marginBottom: '5px', fontWeight: 'bold' }}>Please Select Purchased Program</p>
+                    <div style={flexContainer}>
+                        <label>Student's Name:</label> &nbsp; <input style={inputStyle} type="text" />
+                    </div>
+                    <div style={flexContainer}>
+                        <label>Parent's Name:</label> &nbsp; <input style={inputStyle} type="text" />
+                    </div>
+                    <div style={flexContainer}>
+                        <label>Address:</label> &nbsp; <input style={inputStyle} type="text" />
+                    </div>
+                    <div style={flexContainer}>
+                        City: &nbsp; <input style={inputStyle} type="text" /> &nbsp;
+                        State: &nbsp; <input style={inputStyle} type="text" /> &nbsp;
+                        Zip Code: &nbsp; <input style={inputStyle} type="text" />
+                    </div>
+                    <div style={flexContainer}>
+                        <label>Student's Email Address:</label> &nbsp; <input style={inputStyle} type="text" />
+                    </div>
+                    <div style={flexContainer}>
+                        Home Phone: &nbsp; <input style={inputStyle} type="text" /> &nbsp;
+                        &nbsp; Student's Cell Phone: <input style={inputStyle} type="text" /><br />
+                    </div>
+                    <div style={flexContainer}>
+                        <label>Best Time To Call: </label> &nbsp; <input style={inputStyle} type="text" />
+                    </div>
+                    <div style={flexContainer}>
+                        <label>Parent's Phone No:</label> &nbsp; <input style={inputStyle} type="text" />
+                    </div>
+                    <div style={flexContainer}>
+                        <label>Learner's Permit #:</label> &nbsp; <input style={inputStyle} type="text" />
+                    </div>
+                    <div style={flexContainer}>
+                        Date of Birth: &nbsp; <input style={inputStyle} type="text" />
+                        &nbsp;  Starting Date: <input style={inputStyle} type="text" />
+                    </div>
+                    <div style={flexContainer}>
+                        <label>Notes:</label> &nbsp; <input style={inputStyle} type="text" />
+                    </div>
 
-                    <table>
-                        <tr>
+                    <p style={{ marginTop: '5px', marginBottom: '5px', fontWeight: 'bold', textDecoration: 'underline' }}>Please Select Purchased Program</p>
+
+                    <table style={table}>
+                        <tr style={trStyle}>
                             <td>
-                                <div style={{marginBottom: '5px'}}>
-                                    <span>1. Driver's ED Full Program</span>
-                                    <br />
-                                    - 30 Hours of Class Lessons <br />
-                                    - 12 Hours of Driving Lessons<br />
-                                    - 6 Hours of Observations<br />
-                                    - 2 Hours of Parent Class<br />
+                                <div style={{ marginBottom: '5px' }}>
+                                    <span>1. &emsp; Driver's ED Full Program</span>
+                                    <br style={{ marginBottom: '5px' }} />
+                                    &emsp; &emsp; &emsp;a. &emsp; 30 Hours of Class Lessons <br />
+                                    &emsp; &emsp; &emsp;b. &emsp; 12 Hours of Driving Lessons<br />
+                                    &emsp; &emsp; &emsp;c. &emsp; 6 Hours of Observations<br />
+                                    &emsp; &emsp; &emsp;d. &emsp; 2 Hours of Parent Class<br />
                                 </div>
                             </td>
                         </tr>
-                        <tr>
-                            <td>2. 30 Hours of Classroom Lessons</td>
+                        <tr style={trStyle}>
+                            <td>2. &emsp; 30 Hours of Classroom Lessons</td>
                         </tr>
-                        <tr>
-                            <td>3. 12 Hours of Driving Instructions</td>
+                        <tr style={trStyle}>
+                            <td>3. &emsp; 12 Hours of Driving Instructions</td>
                         </tr>
-                        <tr>
-                            <td>4. 6 Hours of Driving Lessons</td>
+                        <tr style={trStyle}>
+                            <td>4. &emsp; 6 Hours of Driving Lessons</td>
                         </tr>
-                        <tr>
-                            <td>5. 1 Hour Lesson</td>
+                        <tr style={trStyle}>
+                            <td>5. &emsp; 1 Hour Lesson</td>
                         </tr>
-                        <tr>
+                        <tr style={trStyle}>
                             <td>
                                 <div>
-                                    6. Road Test Sponsorship Fee  <br />
-                                    - At the Driving School or RMV Taunton, MA Location
+                                    6. &emsp; Road Test Sponsorship Fee  <br />
+                                    &emsp;&emsp; At the Driving School or RMV Taunton, MA Location
 				                </div>
                             </td>
                         </tr>
                     </table>
                 </div>
-                <div style={{ marginTop: '5px', marginBottom: '5px' }}>
-                    <span style={boldFont}>I have read and agree to these terms:</span><br style={{ marginBottom: '3px'}}/>
-                    Student Signature: &nbsp; <input style={inputStyle} type="text" size='49' /> <br />
-                    Parent Signature: &nbsp; <input style={inputStyle} type="text" size='49' /> <br />
-                    Date: &nbsp; <input style={inputStyle} type="text" size='45' /> <br /><br />
-
-                    Office Use only: Date Paid&nbsp;
-                            <input style={inputStyle} type="text" size='18' />
-                            &nbsp; Amount Paid: <input style={inputStyle} type="text" size='10' />
+                <br />
+                <div>
+                    <span style={boldFont}>I have read and agree to these terms:</span><br /><br />
+                    <div style={flexContainer}>
+                        <label>Student Signature:</label> &nbsp; <input style={inputStyle} type="text" />
+                    </div>
+                    <div style={flexContainer}>
+                        <label>Parent Signature:</label> &nbsp; <input style={inputStyle} type="text" />
+                    </div> 
+                    <div style={flexContainer}>
+                        <label>Date:</label> &nbsp; <input style={inputStyle} type="text" />
+                    </div>
+                    <br />
+                   
+                    <div style={flexContainer}>
+                            Office Use only: Date Paid &nbsp;<input style={inputStyle} type="text" />
+                            &nbsp; Amount Paid: <input style={inputStyle} type="text" />
+                    </div>
                     <br /><br />
+                    <br />
                 </div>
             </div>
         );
